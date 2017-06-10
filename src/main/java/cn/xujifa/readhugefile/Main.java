6c90b8210b2b1ca0d7781d9dbdd40b629f67ad6d
xujifa <hustjifa@gmail.com>
2017-06-10 10:05:11 +0800

    init

---
+++ src/main/java/cn/xujifa/readhugefile/Main.java
@@ -0,0 +1,15 @@
+package cn.xujifa.readhugefile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * Created by xujifa on 17-6-10.
+ */
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+
+  }
+}
